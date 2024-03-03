09ee4ce4a0df150c50b9802b00060baaad885bbf
Simatwa <smartwacaleb@gmail.com>
2024-03-03 15:12:28 +0300

    Made with Love

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import pyftpdlib.__main__ as entry
+from sys import exit
 
 try:
     entry.main()
 except Exception as e:
     print(f'^{e.args[1] if e.args and len(e.args) > 1 else e}')
+    exit(1)
